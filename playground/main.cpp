98dab38741f4536168ec2d165f75b9b82f831347
Deep <shecallme3000@gmail.com>
2025-10-13 20:00:36 +0530

    updated .vscode for limited memory g++ compilation

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,6 +1,37 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-  std::cout << "Deep0x1" << std::endl;
+  int n;
+  cin >> n;
+  // First Half
+  for (int i = 0; i < n; i++) {
+    // Left Wing
+    for (int j = 0; j <= i; j++) cout << "*";
+    
+    // Spaces
+    int spaces = 2 * (n - i - 1);
+    for (int j = 0; j < spaces; j++) cout << " ";
+    
+    // Right Wing
+    for (int j = 0; j <= i; j++) cout << "*";
+    
+    cout << "\n";
+  }
+
+  // Second Half
+  for (int i = 0; i < n; i++) {
+    // Left Wing
+    for (int j = 0; j < n - i; j++) cout << "*";
+    
+    // Spaces
+    int spaces = 2 * i;
+    for (int j = 0; j < spaces; j++) cout << " ";
+
+    // Right Wing
+    for (int j = 0; j < n - i; j++) cout << "*";
+
+    cout << "\n";
+  }
   return 0;
 }
